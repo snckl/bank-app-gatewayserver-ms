4c3484e7814341cbb48ae712d0a02a5b4f0a1430
Sinan <sinan.cakal00@gmail.com>
2024-03-04 22:41:37 +0300

    initial commit

---
+++ src/main/java/com/cnsn/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,39 @@
+package com.cnsn.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator bankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p-> p
+						.path("/bank/accounts/**") // if path equals to
+						.filters( f -> f.rewritePath("/bank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS")) // forward request to actual microservice
+				.route(p -> p
+						.path("/bank/cards/**")
+						.filters( f -> f.rewritePath("/bank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS"))
+				.route(p -> p
+						.path("/bank/loans/**")
+						.filters( f -> f.rewritePath("/bank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS")).build();
+
+	}
+
+}
---
+++ src/main/java/com/cnsn/gatewayserver/filters/RequestTraceFilter.java
@@ -0,0 +1,46 @@
+package com.cnsn.gatewayserver.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Order(1) // this filter runs first
+@Component
+public class RequestTraceFilter implements GlobalFilter {
+    private static final Logger logger = LoggerFactory.getLogger(RequestTraceFilter.class);
+
+    @Autowired
+    private FilterUtility filterUtility;
+
+    // exchange is basically request response - Filter chain already self-explanatory
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        if(isCorrelationIdPresent(requestHeaders)){
+            logger.debug("bank-correlation-id found in RequestTraceFilter : {}",
+                    filterUtility.getCorrelationId(requestHeaders));
+
+        } else {
+            String correlationID = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange,correlationID);
+            logger.debug("bank-correlation-id generated in RequestTraceFilter : {}",
+                    correlationID);
+        }
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders){
+        return filterUtility.getCorrelationId(requestHeaders) != null;
+    }
+
+    private String generateCorrelationId(){
+        return java.util.UUID.randomUUID().toString();
+    }
+}
